8d2aaa2cea310ffb986ba128c17c56901007100f
William Lin <wlsaidhi@gmail.com>
2023-11-08 13:23:00 -0800

    update script

--- benchmarks/python/script/default_bench.sh
+++ benchmarks/python/script/default_bench.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -x
+#set -x
 for b_size in 1 16 32 64
 do
     for in_len in 128 2048
@@ -9,11 +9,11 @@ do
                 #echo $b_size
                 #echo $in_len
                 #echo $out_len
-                echo "===="
-                benchmark.py -m llama_7b --mode plugin
+                #echo "===="
+                python3 benchmark.py -m llama_7b --mode plugin \
                 --batch_size "$b_size" \
-                --input_output_len "128,128" --max_input_len 2048 \
-                --max_output_len 2048
+                --input_output_len "$in_len,$out_len" --max_input_len $in_len \
+                --max_output_len $out_len --enable_fp8 --fp8_kv_cache
 
             done
     done
